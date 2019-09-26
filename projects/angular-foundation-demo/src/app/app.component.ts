0f2059f75f7dd7eef24c2f4ea6413e10835c02c2
Eric Gibby <ericgibby@users.noreply.github.com>
2019-09-26 16:53:03 -0600

    Wired up menu component in demo app

--- projects/angular-foundation-demo/src/app/app.component.ts
+++ projects/angular-foundation-demo/src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from '../../../angular-foundation/src/lib/menu/menu-item';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	items: MenuItem[];
+
+	constructor(private router: Router) {}
+
+	handleClick(event: Event, href: string) {
+		event.preventDefault();
+		event.stopPropagation();
+
+		this.router.navigateByUrl(href);
+	}
+
+	ngOnInit() {
+		this.items = [{ href: '/', text: 'Home', onClick: this.handleClick.bind(this) }];
+	}
+}
--- projects/angular-foundation-demo/src/app/app.module.ts
+++ projects/angular-foundation-demo/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFoundationModule } from '../../../angular-foundation/src/lib/angular-foundation.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, AngularFoundationModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
