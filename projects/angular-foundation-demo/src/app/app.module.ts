7f58a6595a3be61364ec2b7abce57567e37f8e4b
Eric Gibby <ericgibby@users.noreply.github.com>
2019-09-27 09:28:15 -0600

    Added grid component

--- projects/angular-foundation-demo/src/app/app.module.ts
+++ projects/angular-foundation-demo/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFoundationModule } from '../../../angular-foundation/src/lib/angular-foundation.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './views/grid/grid.component';
 import { MenuComponent } from './views/menu/menu.component';
 
 @NgModule({
-	declarations: [AppComponent, MenuComponent],
+	declarations: [AppComponent, GridComponent, MenuComponent],
 	imports: [BrowserModule, AppRoutingModule, AngularFoundationModule],
 	providers: [],
 	bootstrap: [AppComponent]
