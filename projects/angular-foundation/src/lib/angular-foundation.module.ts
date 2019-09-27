cf794509f5b962c663b4f5d9c3e292caa3b2313d
Eric Gibby <ericgibby@users.noreply.github.com>
2019-09-27 13:42:05 -0600

    Generated grid component

--- projects/angular-foundation/src/lib/angular-foundation.module.ts
+++ projects/angular-foundation/src/lib/angular-foundation.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { GridComponent } from './grid/grid.component';
 
-const libraryComponents = [MenuComponent];
+const libraryComponents = [GridComponent, MenuComponent];
 
 @NgModule({
 	declarations: [...libraryComponents],
