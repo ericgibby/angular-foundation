08448cbd34fafd0ffdde72a4eb010bb80717fdef
Eric Gibby <ericgibby@users.noreply.github.com>
2019-09-27 14:21:58 -0600

    Added grid component sample

--- projects/angular-foundation-demo/src/app/views/grid/grid.component.ts
+++ projects/angular-foundation-demo/src/app/views/grid/grid.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./grid.component.scss']
 })
 export class GridComponent implements OnInit {
+	columns = [
+		{ key: 'firstName', title: 'First Name' },
+		{ key: 'lastName', title: 'Last Name' },
+		{ key: 'email', title: 'Email' }
+	];
+	data = [
+		{ firstName: 'Tony', lastName: 'Stark', email: 'ironman@avenge.rs' },
+		{ firstName: 'Steve', lastName: 'Rogers', email: 'cap@avenge.rs' }
+	];
+
+	codeSnippet = `this.columns: GridColumn[] = [
+	{ key: 'firstName', title: 'First Name' },
+	{ key: 'lastName', title: 'Last Name' },
+	{ key: 'email', title: 'Email' }
+];
+
+this.data: any[] = [
+	{ firstName: 'Tony', lastName: 'Stark', email: 'ironman@avenge.rs' },
+	{ firstName: 'Steve', lastName: 'Rogers', email: 'cap@avenge.rs' }
+];`;
+	htmlSnippet = `<af-grid [columns]="columns" [data]="data"></af-grid>`;
+
 	constructor() {}
 
 	ngOnInit() {}
