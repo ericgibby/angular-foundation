087f9521cba0ac8174b1d6a7cf2833c5f05455c1
Eric Gibby <ericgibby@users.noreply.github.com>
2019-09-27 09:32:17 -0600

    Added menu view to demo app

--- projects/angular-foundation-demo/src/app/app.module.ts
+++ projects/angular-foundation-demo/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFoundationModule } from '../../../angular-foundation/src/lib/angular-foundation.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './views/menu/menu.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, MenuComponent],
 	imports: [BrowserModule, AppRoutingModule, AngularFoundationModule],
 	providers: [],
 	bootstrap: [AppComponent]
