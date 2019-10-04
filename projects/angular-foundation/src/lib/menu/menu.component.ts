ae8f924b9b165d35752290a512efd728dc7fb93e
Eric Gibby <ericgibby@users.noreply.github.com>
2019-10-04 16:52:48 -0600

    Updated components to extend `StatefulComponent`

--- projects/angular-foundation/src/lib/menu/menu.component.ts
+++ projects/angular-foundation/src/lib/menu/menu.component.ts
@@ -1,22 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { StatefulComponent } from '../stateful-component';
+import { StatefulInput } from '../stateful-input.decorator';
 import { MenuItem } from './menu-item';
+
+class MenuState {
+	hasItems: boolean;
+	items: MenuItem[];
+}
 
 @Component({
 	selector: 'af-menu',
 	templateUrl: './menu.component.html',
 	styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-	@Input() items: MenuItem[];
+export class MenuComponent extends StatefulComponent<MenuState> implements OnChanges {
+	@StatefulInput<MenuState>() @Input() items: MenuItem[] | string;
 
-	constructor() {}
-
-	ngOnInit() {}
+	constructor(protected cd: ChangeDetectorRef) {
+		super(cd);
+	}
 
 	handleClick(event: Event, item: MenuItem) {
 		const { href, onClick } = item;
 		if (onClick) {
 			onClick(event, href);
+		}
+	}
+
+	ngOnChanges(changes: SimpleChanges) {
+		const hadItems = !!this.state.hasItems;
+		super.ngOnChanges(changes);
+		if (hadItems !== !!this.state.items) {
+			this.setState({ hasItems: !!this.state.items });
 		}
 	}
 }
--- projects/angular-foundation/src/lib/table/table.component.ts
+++ projects/angular-foundation/src/lib/table/table.component.ts
@@ -1,16 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { StatefulComponent } from '../stateful-component';
+import { StatefulInput } from '../stateful-input.decorator';
 import { TableColumn } from './table-column';
+
+class TableState {
+	columns: TableColumn[];
+	data: any[];
+}
 
 @Component({
 	selector: 'af-table',
 	templateUrl: './table.component.html',
 	styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
-	@Input() columns: TableColumn[];
-	@Input() data: any[];
+export class TableComponent extends StatefulComponent<TableState> {
+	@StatefulInput<TableState>() @Input() columns: TableColumn[] | string;
+	@StatefulInput<TableState>() @Input() data: any[] | string;
 
-	constructor() {}
-
-	ngOnInit() {}
+	constructor(protected cd: ChangeDetectorRef) {
+		super(cd);
+	}
 }
