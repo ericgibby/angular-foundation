743ecf864bd65f0aaf27c57cf28d6d49bac82985
Eric Gibby <ericgibby@users.noreply.github.com>
2019-10-07 14:45:50 -0600

    Export AF_COMPONENTS variable

--- projects/angular-foundation/src/public-api.ts
+++ projects/angular-foundation/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/angular-foundation.module';
 export * from './lib/angular-foundation-elements.module';
+export { AF_COMPONENTS } from './lib/shared.module';
 
 // menu
 export * from './lib/menu/menu-item';
